db3de254361cd14f5e199e6609d7f9778352a59e
carrot <reeshul@hotmail.com>
2020-01-07 15:16:05 +0000

    broken homepage into smaller components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import HomePage from './HomePageComponent'
+import HomePage from './pages/homepage/HomePageComponent'
 
 function App() {
   return (
