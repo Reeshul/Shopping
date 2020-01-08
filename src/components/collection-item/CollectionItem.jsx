3dd389dbdca19b568e67716777c4bea422fab96a
carrot <reeshul@hotmail.com>
2020-01-08 14:28:55 +0000

    shop page, collection preview and item component

---
+++ src/components/collection-item/CollectionItem.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import './CollectionItemStyles.scss';
+
+const CollectionItem = ({ id, name, price, imageUrl}) => (
+  <div className='collection-item'>
+    <div className='image'
+        style={{
+            backgroundImage: `url(${imageUrl})`
+        }}
+    />
+
+        <div className='collection-footer' ></div>
+            <span className='name'>{name}</span>
+            <span className='price'>{price}</span>
+    
+  </div>
+);
+
+export default CollectionItem;
