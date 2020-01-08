8e7ff86d61d7e2fdf37426cf321c8442df7367c3
carrot <reeshul@hotmail.com>
2020-01-08 15:02:28 +0000

    header component for navigation links

--- src/App.js
+++ src/App.js
@@ -4,20 +4,17 @@ import './App.css';
 
 import HomePage from './pages/homepage/HomePageComponent';
 import ShopPage from './pages/shop/ShopPage'
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-)
+import Header from './components/header/Header';
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
       <Route exact path='/' component={HomePage} />
       <Route path='/shop' component={ShopPage} />
       </Switch>
+      
       
     </div>
   );
